dca3fcf4bcc2645e9a4acb3231ba80db70f83884
AndreRibeiroAlves <andrehallwas12@hotmail.com>
2019-09-11 09:21:23 -0300

    Updates

---
+++ precache-manifest.188f8d22153213f60f082b6a9c254ff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e875c72e065fcc28642b4d19d1955c30",
+    "url": "/DashboardReact/index.html"
+  },
+  {
+    "revision": "63e67655f1ed6796d52e",
+    "url": "/DashboardReact/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "31d198f3a8f03ca7da37",
+    "url": "/DashboardReact/static/js/2.d70638b7.chunk.js"
+  },
+  {
+    "revision": "63e67655f1ed6796d52e",
+    "url": "/DashboardReact/static/js/main.55eace49.chunk.js"
+  },
+  {
+    "revision": "967fdc4aaf1eb95117c9",
+    "url": "/DashboardReact/static/js/runtime~main.16ce599c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/DashboardReact/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DashboardReact/precache-manifest.1f41ce63ba178d3ced8e7a5a2ddf57c3.js"
+  "/DashboardReact/precache-manifest.188f8d22153213f60f082b6a9c254ff3.js"
 );
 
 self.addEventListener('message', (event) => {
