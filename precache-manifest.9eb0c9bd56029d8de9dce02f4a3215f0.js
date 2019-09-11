6dab50681c4f71e639bab0ae8c42883906fe2032
Your Name <email@emqil.com>
2019-09-10 23:25:37 -0300

    Updates

---
+++ precache-manifest.9eb0c9bd56029d8de9dce02f4a3215f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79371e33b0fe959886fc9769d80c9580",
+    "url": "/DashboardReact/index.html"
+  },
+  {
+    "revision": "7c42f268926d3b3020a7",
+    "url": "/DashboardReact/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4a023363dc241b71634d",
+    "url": "/DashboardReact/static/js/2.0bf09cc4.chunk.js"
+  },
+  {
+    "revision": "7c42f268926d3b3020a7",
+    "url": "/DashboardReact/static/js/main.f1b37a8a.chunk.js"
+  },
+  {
+    "revision": "967fdc4aaf1eb95117c9",
+    "url": "/DashboardReact/static/js/runtime~main.16ce599c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DashboardReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
