55f9e23f02e26ca42c1a201151b2725b0712527b
AndreRibeiroAlves <andrehallwas12@hotmail.com>
2019-10-09 11:32:53 -0300

    Teste 2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from 'react'
 import Leaflet from 'leaflet';
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 import './leaflet/dist/leaflet.css';
-import Sidebar from "react-sidebar";
+import Sidebar from 'leaflet-sidebar'
+import L from 'leaflet';
 
+///https://dev.to/itsmestevieg/customising-leafletjs-f55
+///https://leafletjs.com/plugins.html
+///https://jsfiddle.net/wcLvsg37/1/
+///https://stackoverflow.com/questions/42282998/leaflet-sidebar-display-data-in-the-sidebar-when-a-marker-is-clicked
+///https://embed.plnkr.co/plunk/wH7u64
+///https://stackoverflow.com/questions/50437852/leaflet-sidebar-within-a-while-loop
+///https://stackoverflow.com/questions/36860914/toggle-leaflet-sidebar-v2?rq=1
+///https://www.npmjs.com/package/leaflet-sidebar
+///https://stackoverflow.com/questions/54047240/leaflet-js-with-tabletop-js-sidebar-menu-for-map
+///https://codepen.io/danhahn/pen/QKwEXN
 
 Leaflet.Icon.Default.imagePath =
 '../node_modules/leaflet'
@@ -16,106 +27,41 @@ Leaflet.Icon.Default.mergeOptions({
     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
-class Bar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sidebarOpen: true
-    };
-    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-  }
- 
-  onSetSidebarOpen(open) {
-    this.setState({ sidebarOpen: open });
-  }
- 
-  render() {
-    return (
-	  <div>
-		  <Sidebar
-			sidebar={<b> bar </b>}
-			open={this.state.sidebarOpen}
-			onSetOpen={this.onSetSidebarOpen}
-			styles={{ sidebar: { background: "white" } }}
-		  >
-			<button onClick={() => this.onSetSidebarOpen(true)}>
-			  Open sidebar
-			</button>
-		  </Sidebar>
-	  </div>
-    );
-  }
-}
+class Map extends React.Component {
+  componentDidMount() {
+	  
+    var map = L.map('map', {
+      center: [-22.1225167, -51.3882528],
+      zoom: 16,
+      layers: [
+        L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png', {
+          attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, © <a href="https://carto.com/attribution">CARTO</a>',
+          maxNativeZoom:19,
+		  maxZoom: 20
+        }),
+      ]
+    });
 
-class Map extends React.Component {constructor(props) {
-  super(props);
-	this.state = {
-	  sidebarOpen: false
-	};
-    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-  }
- 
-  onSetSidebarOpen(open) {
-    this.setState({ sidebarOpen: open });
-  }
-  
-  render() {
+	var sidebar = L.control.sidebar('sidebar', {
+		closeButton: true,
+		position: 'left'
+	});
+	map.addControl(sidebar);
+
+	setTimeout(function () {
+		sidebar.show();
+	}, 500);
 	
-    return (
-	    <div>
-		  <LeafletMap
-			center={[-22.1225167, -51.3882528]}
-			zoom={16}
-			maxZoom={20}
-			attributionControl={true}
-			zoomControl={true}
-			doubleClickZoom={true}
-			scrollWheelZoom={true}
-			dragging={true}
-			animate={true}
-			easeLinearity={0.35}
-			style={{flexItems: 1, position: 'absolute', top: 0, bottom: 0, left: 0, right: 0}}
-		  >		
-			<TileLayer
-			  url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' 
-			  attribution= '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' 
-			/>
-			<Marker position={[-22.1225167, -51.3862528]} >
-			  <Popup>
-				<button onClick={() => this.onSetSidebarOpen(true)}>
-				  Open sidebar
-				</button>
-			  </Popup>
-			</Marker>
-			<Marker position={[-22.1226167, -51.3878528]}>
-			  <Popup>
-				Sensor.
-			  </Popup>
-			</Marker>
-			<Marker position={[-22.1215167, -51.3842528]}>
-			  <Popup>
-				Sensor.
-			  </Popup>
-			</Marker>
-			<Marker position={[-22.1236167, -51.3742528]}>
-			  <Popup>
-				Sensor.
-			  </Popup>
-			</Marker>
-			<Marker position={[-22.1285167, -51.3882528]}>
-			  <Popup>
-				Sensor.
-			  </Popup>
-			</Marker>
-		  </LeafletMap>
-		  <Sidebar
-		    sidebar={<b> bar </b>}
-		    open={this.state.sidebarOpen}
-		    onSetOpen={this.onSetSidebarOpen}
-		    styles={{ sidebar: { background: "white" } }}
-		  />
-		</div>
-    );
+	var marker = L.marker([49.46, 17.11]).addTo(map).on('click', function () {
+		sidebar.toogle()
+		marker.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+	});
+
+  }
+
+  render() {
+    return <div style={{height: 600}}id="map"></div>
   }
 }
 
